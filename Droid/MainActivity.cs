3a7c97fff56ac32ff59bd5775a7c873de64adfcf
goranhalvarsson@gmail.com <goranhalvarsson@gmail.com>
2016-04-12 17:41:30 +0200

    * HabitatApp.Droid.csproj: Added icons

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace HabitatApp.Droid
 {
-	[Activity (Label = "HabitatApp.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "Habitat App", Icon = "@drawable/habitatIcon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
